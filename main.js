b7c6f7ee4d4a354083e6528405a3b815e6ef2108
rob g <cvrobg@gmail.com>
2026-03-03 16:40:47 +0800

    >992px changes done

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 });
+
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.querySelector(".contact-btn");
+
+    if (btn) {
+      btn.addEventListener("click", function () {
+        const user = this.dataset.user;
+        const domain = this.dataset.domain;
+        const tld = this.dataset.tld;
+
+        const email = user + "@" + domain + "." + tld;
+
+        const link = document.createElement("a");
+        link.href = "mailto:" + email;
+        link.click();
+      });
+    }
+  });
